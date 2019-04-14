c6edaaea1580e50670df866fcea2fefda24ee610
Miki Tebeka <miki.tebeka@gmail.com>
2019-04-14 15:51:32 +0300

    Using regular expression doctest

--- nlp.py
+++ nlp.py
@@ -1,3 +1,10 @@
+import re
+
+
 def tokenize(text):
-    """Tokenize text to words"""
-    return text.split()
+    """Tokenize text to words
+
+    >>> tokenize("How are you?")
+    ['How', 'are', 'you']
+    """
+    return re.findall(r'[a-zA-Z]+', text)
